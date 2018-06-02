8490b2b7b064831bf68b0f8cb277b8b778db3596
Yu-Hung Lin <yuhung@yuhunglin.com>
2018-06-02 07:40:57 -0700

    v1.0.3
    `docker-openvpn-server/cluster`: Optional output for EIP
    - worked through https://www.terraform.io/upgrade-guides/0-11.html#referencing-attributes-from-resources-with-count-0
    to get eip alloc id output

--- docker-openvpn-server/cluster/outputs.tf
+++ docker-openvpn-server/cluster/outputs.tf
@@ -6,3 +6,7 @@ output "vpn_server_sg_id" {
 output "role_id_openvpn" {
   value = "${aws_iam_role.role.unique_id}"
 }
+
+output "vpn_server_eip_id" {
+  value = "${element(concat(aws_eip.openvpn_eip.*.id, list("")), 0)}"
+}
